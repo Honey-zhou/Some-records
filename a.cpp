d2a88d68e8e1a9c32820f151f370df62cb7dd075
honey-zhou <2354442496@qq.com>
2021-03-06 18:50:09 +0800

    commit times of local branch main : 1

---
+++ a.cpp
@@ -0,0 +1,14 @@
+#include<iosteram>
+
+int main()
+{
+   using std::cout;
+   using std::endl;
+   
+   for（int i = 0 ; i++ ; i < 5）{
+       cout << "Hello! 2021"<< endl;
+   } 
+  
+   return 0;
+}
+
